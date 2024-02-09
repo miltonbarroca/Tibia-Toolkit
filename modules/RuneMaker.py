6bf431196deeec22c7db39ef93f0f076ca68d429
Milton Barroca <miltonbarroca@gmail.com>
2024-02-09 16:22:49 -0300

    creating buttons

--- modules/RuneMaker.py
+++ modules/RuneMaker.py
@@ -1,9 +1,15 @@
-import tkinter
+from tkinter.ttk import Label
+from ttkthemes import ThemedTk
 
-root = tkinter.Tk()
+root = ThemedTk(theme="black", themebg=True)
 root.title("MyInterface")
 root.geometry("500x500+250+250")
 root.resizable(False,False)
 
+lbl_food = Label(text="Hotkey Eat Food")
+lbl_food.grid(row=0,column=0,padx=10, pady=10)
+
+lbl_cast = Label(text="Hotkey Spell")
+lbl_cast.grid(row=1,column=0)
 
 root.mainloop()
