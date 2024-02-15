29ac40a1ba3689c8304b446dd9224d943a4f4f4f
Milton Barroca <miltonbarroca@gmail.com>
2024-02-14 22:26:35 -0300

    thread event done

--- modules/RuneMaker.py
+++ modules/RuneMaker.py
@@ -8,7 +8,7 @@ import pyautogui
 from conf.window import hidden_client
 import json
 import threading
-import pynput.keyboard
+import pynput
 import time
 
 HOTKEYS = ['off', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12']
@@ -96,7 +96,7 @@ def save():
     with open('infos.json','w') as file:
         file.write(json.dumps(my_data))
 
-btn_start = generate_widget(Button,row=4,column=1,text="Start",command=save)
+
 
 def load():
     with open('infos.json', 'r') as file:
@@ -109,44 +109,30 @@ def load():
 btn_load = generate_widget(Button,row=4,column=0,text="Load",command=load)
 
 def run():
-    wait_to_eat_food = 10
-    time_food = time.time()
+ 
     while not myEvent.is_set():
-        if 'mana_pos' in data and data['mana_pos']['position'] is not None:
-            x = data['mana_pos']['position'][0]
-            y = data['mana_pos']['position'][1]
-            if pyautogui.pixelMatchesColor(x, y, tuple(data['mana_pos']['rgb'])):
-                if data['spell']['value'] != 'Desligado':
-                    pyautogui.press(data['spell']['value'])
-            if data['food']['value'] != 'Desligado':
-                if int(time.time() - time_food) >= wait_to_eat_food:
-                    print('comendo food')
-                    pyautogui.press(data['food']['value'])
-                    time_food = time.time()
-    print('bot parado')
+        print(myEvent.is_set())
+
 
 def key_code(key):
     if key == pynput.keyboard.Key.esc:
-        myEvent.set()
-        root.deiconify()
+        myEvent.set
         return False
 
-def listener_keyboard():
-    with pynput.keyboard.Listener(on_press=key_code, on_release=key_code) as listener:
-        listener.join()
-    
 def start():
     global data
-    root.iconify()
-    save()
     data = load()
     global myEvent
     myEvent = threading.Event()
     global start_th
     start_th = threading.Thread(target=run)
-    keyboard_th = threading.Thread(target=listener_keyboard)
-    keyboard_th.start()
+    start_th.start()
 
+    with pynput.keyboard.Listener(on_press=key_code) as listener:
+        listener.join()
+
+        
+btn_start = generate_widget(Button,row=4,column=1,text="Start",command=start)
 
 
 
