f5a46bdfff0a8e7a778843e1a5b2434a1b82d55f
Milton Barroca <miltonbarroca@gmail.com>
2024-02-16 14:17:37 -0300

    update autocombo

--- modules/AutoCombo.py
+++ modules/AutoCombo.py
@@ -2,7 +2,7 @@ import threading
 import pyautogui as pg
 import time
 import keyboard
-from modules.conf import Constants
+from conf import Constants
 
 pause_programa = False
 finalizar_programa = False
@@ -52,3 +52,29 @@ def pause():
 def set_finalizar_programa(value):
     global finalizar_programa
     finalizar_programa = value
+
+
+# Esperar pela tecla '=' antes de começar
+print('Aguardando pela tecla "=" para iniciar...')
+while True:
+    if keyboard.is_pressed('='):
+        break
+
+# Iniciar threads após a tecla '=' ser pressionada
+thread_combo_atk = threading.Thread(target=auto_combo)
+thread_pause = threading.Thread(target=pause)
+
+thread_combo_atk.start()
+thread_pause.start()
+
+# Aguardar até que o usuário pressione 'o' para finalizar o programa
+while True:
+    if keyboard.is_pressed('o'):
+        set_finalizar_programa(True)
+        break
+
+# Aguardar até que as threads terminem
+thread_combo_atk.join()
+thread_pause.join()
+
+print('Programa finalizado')
