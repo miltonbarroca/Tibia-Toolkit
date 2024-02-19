58675d386df797081012ef2c4e35a70d72336121
Milton Barroca <miltonbarroca@gmail.com>
2024-02-19 10:23:59 -0300

    AutoEquip updated

--- modules/AutoEquip.py
+++ modules/AutoEquip.py
@@ -10,10 +10,10 @@ def check_ring():
         pg.press('j') #hotkey do ring
         time.sleep(random.uniform(0.2, 1))
 
-def check_ring():
-    pixel_ring = (1768, 248)
-    cor = (71, 74, 77)
+def check_amulet():
+    pixel_amulet = (1769, 179)
+    cor = (82, 84, 87)
 
-    while pg.pixelMatchesColor(*pixel_ring, cor):
-        pg.press('j') #hotkey do ring
-        time.sleep(random.uniform(0.2, 1))
+    while pg.pixelMatchesColor(*pixel_amulet, cor):
+        pg.press('k') #hotkey do amuleto
+        time.sleep(random.uniform(0.4, 1))
