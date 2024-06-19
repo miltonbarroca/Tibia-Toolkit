1a962409b6ad86c3916e294460534315a8854a10
milton <miltonbarroca@gmail.com>
2024-06-19 18:19:23 -0300

    fixin check status threads

--- modules/TTK.py
+++ modules/TTK.py
@@ -31,29 +31,20 @@ def kill_box():
         pg.press('space')
         pg.press('7')
         pg.press('9')
-        pg.press('3')
-        pg.press('2')
         if not actions.check_battle() or event_th.is_set() or actions.check_player():
             return
         time.sleep(random.uniform(2, 2.5))
         pg.press('8')
-        pg.press('3')
-        pg.press('2')
         if not actions.check_battle() or event_th.is_set() or actions.check_player():
             return
         time.sleep(random.uniform(2, 2.5))
         pg.press('9')
-        pg.press('3')
-        pg.press('2')
         if not actions.check_battle() or event_th.is_set() or actions.check_player():
             return
         time.sleep(random.uniform(2, 2.5))
         pg.press('0')
-        pg.press('3')
-        pg.press('2')
         pg.press('space')
         pg.press('7')
-        pg.press('3')
         if not actions.check_battle() or event_th.is_set() or actions.check_player():
             return
         time.sleep(random.uniform(2, 2.5))
@@ -70,28 +61,29 @@ def run():
                     if event_th.is_set():
                         return
                     while actions.check_battle():
-                        pg.sleep(1)
+                        time.sleep(1)
                         pg.press('4')
                         kill_box()
                         if event_th.is_set():
                             return
-                        pg.sleep(1)
+                        time.sleep(1)
                         actions.get_loot()
                         if event_th.is_set():
                             return
                         actions.check_ring()
-                        pg.sleep(1)
+                        time.sleep(1)
                         actions.check_amulet()
-                        pg.sleep(1)
+                        time.sleep(1)
                         pg.press('i')
+                        pg.press('s')
                     actions.next_box(item['path'], item['wait'])
-                    pg.sleep(1)    
+                    time.sleep(1)
                     pg.press('i')
                     actions.hole_down(item['down_hole'])
                     if event_th.is_set():
                         return
-                    actions.hole_up(item['up_hole'],'img/stair_up.png',1,1)
-                    pg.sleep(1)
+                    actions.hole_up(item['up_hole'], 'img/stair_up.png', 1, 1)
+                    time.sleep(1)
                     pg.press('i')
                     if event_th.is_set():
                         return
@@ -100,25 +92,37 @@ def run():
     except Exception as e:
         logging.error(f"Erro durante a execução geral: {e}")
 
-def key_code(key): #key_code(key, th_group)
+def key_code(key):
     if key == keyboard.Key.esc:
         event_th.set()
-        #th_group.stop()
         return False
     if key == keyboard.Key.delete:
-        #th_group.start()
-        th_run.start()
+        event_th.clear()
+        if not th_run.is_alive():
+            th_run.start()
+        start_check_threads()
+
+def check_status(name, delay, x, y, rgb, button_names):
+    while not event_th.is_set():
+        if not pg.pixelMatchesColor(x, y, rgb):
+            for button_name in button_names:
+                pg.press(button_name)
+                time.sleep(delay)
+
+def start_check_threads():
+    global th_check_mana, th_check_life, th_check_exura
+    th_check_mana = threading.Thread(target=check_status, args=('mana', 2.1, *Constants.PIXEL_MANA, Constants.COR_MANA, ['3']))
+    th_check_life = threading.Thread(target=check_status, args=('life', 1, *Constants.PIXEL_LIFE, Constants.COR_LIFE, ['1']))
+    th_check_exura = threading.Thread(target=check_status, args=('exura', 1.9, *Constants.PIXEL_EXURA, Constants.COR_EXURA, ['2']))
+
+    th_check_mana.start()
+    th_check_life.start()
+    th_check_exura.start()
 
 global event_th
 event_th = threading.Event()
+
 th_run = threading.Thread(target=run)
 
-#th_check_mana = my_thread.MyThread(lambda: Actions.check_status('mana',2.2, *Constants.PIXEL_MANA, Constants.COR_MANA, '3'))
-#th_check_life = my_thread.MyThread(lambda : Actions.check_status('life',1,*Constants.PIXEL_LIFE,Constants.COR_LIFE,'1'))
-#th_check_exura = my_thread.MyThread(lambda : Actions.check_status('exura',2,*Constants.PIXEL_EXURA,Constants.COR_EXURA,'2'))
-#th_check_utamo = my_thread.MyThread(lambda : Actions.check_status('utamo',1.2,*Constants.PIXEL_LIFE,Constants.COR_LIFE,'6'))
-
-#group_threads = my_thread.ThreadGroup([th_check_mana,th_check_life,th_check_exura])
-
-with Listener(on_press=lambda key: key_code(key)) as listener : #, key_code(key,group_threads)
+with Listener(on_press=key_code) as listener:
     listener.join()
