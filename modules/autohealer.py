280fe3cd426558bc1d50272112ec5833aabd846d
Mazukz <nigrefernandes@gmail.com>
2024-06-29 16:30:37 +1000

    Fix some UI bugs, Implemented key listening by ttk.bind, now being able to use F1-F12 as hotkey
    Improved logic on manager_supplies reducing repetition and improving readability

--- modules/autohealer.py
+++ modules/autohealer.py
@@ -21,22 +21,19 @@ def pixel_matches_color(region, percent, color):
     pixel_color = screenshot.getpixel((result_width, region[3] - 1))
     return pixel_color == color
 
-def manager_suplies(event, pause_event, life_threshold, life_button, exura_threshold, exura_button, mana_threshold, mana_button):
-    while not event.is_set():
-        if not pixel_matches_color(LIFE_REGION, life_threshold, LIFE_COLOR):
-            pg.press(life_button)
-        if event.is_set():
-            return
-        if not pixel_matches_color(LIFE_REGION, exura_threshold, LIFE_COLOR):
-            pg.press(exura_button)
-        if not pixel_matches_color(MANA_REGION, mana_threshold, MANA_COLOR):
-            pg.press(mana_button)
-        if event.is_set():
-            return
-        while pause_event.is_set():
-            if event.is_set():
-                return
-            pass
+def check_and_press(region, threshold, color, button):
+    if not pixel_matches_color(region, threshold, color):
+        pg.press(button)
+
+def manager_supplies(stop_event, pause_event, life_threshold, life_button, exura_threshold, exura_button, mana_threshold, mana_button):
+    while not stop_event.is_set():
+        if pause_event.is_set():
+            stop_event.wait()
+            continue
+
+        check_and_press(LIFE_REGION, life_threshold, LIFE_COLOR, life_button)
+        check_and_press(LIFE_REGION, exura_threshold, LIFE_COLOR, exura_button)
+        check_and_press(MANA_REGION, mana_threshold, MANA_COLOR, mana_button)
 
 class App:
     def __init__(self, root):
@@ -68,34 +65,34 @@ class App:
         entry_font = ('Helvetica', 12)
 
         ttk.Label(self.root, text="Life Threshold:", font=label_font).pack(pady=(10, 0))
-        self.life_threshold = tk.StringVar(self.root)
-        self.life_threshold.set(options[6])  # default value is 70
+        self.life_threshold = tk.IntVar(self.root)
         ttk.OptionMenu(self.root, self.life_threshold, *options).pack(pady=(0, 10))
+        self.life_threshold.set(options[6])  # default value is 70
 
         ttk.Label(self.root, text="Life Button:", font=label_font).pack(pady=(10, 0))
         self.life_button = ttk.Entry(self.root, font=entry_font)
         self.life_button.pack(pady=(0, 10))
-        self.life_button.insert(0, "2")
+        self.life_button.bind("<KeyPress>", lambda event, widget=self.life_button: self.on_key_press(event, widget))
 
         ttk.Label(self.root, text="Exura Threshold:", font=label_font).pack(pady=(10, 0))
-        self.exura_threshold = tk.StringVar(self.root)
-        self.exura_threshold.set(options[3])  # default value is 40
+        self.exura_threshold = tk.IntVar(self.root)
         ttk.OptionMenu(self.root, self.exura_threshold, *options).pack(pady=(0, 10))
+        self.exura_threshold.set(options[3])  # default value is 40
 
         ttk.Label(self.root, text="Exura Button:", font=label_font).pack(pady=(10, 0))
         self.exura_button = ttk.Entry(self.root, font=entry_font)
         self.exura_button.pack(pady=(0, 10))
-        self.exura_button.insert(0, "1")
+        self.exura_button.bind("<KeyPress>", lambda event, widget=self.exura_button: self.on_key_press(event, widget))
 
         ttk.Label(self.root, text="Mana Threshold:", font=label_font).pack(pady=(10, 0))
-        self.mana_threshold = tk.StringVar(self.root)
-        self.mana_threshold.set(options[8])  # default value is 80
+        self.mana_threshold = tk.IntVar(self.root)
         ttk.OptionMenu(self.root, self.mana_threshold, *options).pack(pady=(0, 10))
+        self.mana_threshold.set(options[8])  # default value is 80
 
         ttk.Label(self.root, text="Mana Button:", font=label_font).pack(pady=(10, 0))
         self.mana_button = ttk.Entry(self.root, font=entry_font)
         self.mana_button.pack(pady=(0, 20))
-        self.mana_button.insert(0, "3")
+        self.mana_button.bind("<KeyPress>", lambda event, widget=self.mana_button: self.on_key_press(event, widget))
 
         button_frame = ttk.Frame(self.root)
         button_frame.pack(pady=20)
@@ -116,18 +113,30 @@ class App:
         signature_label = ttk.Label(self.root, text="Tibia Tool Kit", font=signature_font)
         signature_label.pack(side="bottom", pady=(5, 5))
 
+    def on_key_press(self, event, target_widget):
+        target_widget.delete(0, tk.END)
+        if event.keysym in ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12']:
+            target_widget.insert(0, event.keysym)
+        
     def start(self):
+
+        if not all([self.life_button.get(), self.exura_button.get(), self.mana_button.get()]):
+            print("Error: One or more button inputs are empty.")
+            return 
+
         if self.thread is None or not self.thread.is_alive():
             self.stop_event.clear()
             self.pause_event.clear()
+            
             life_threshold = int(self.life_threshold.get())
             life_button = self.life_button.get()
             exura_threshold = int(self.exura_threshold.get())
             exura_button = self.exura_button.get()
             mana_threshold = int(self.mana_threshold.get())
             mana_button = self.mana_button.get()
-            self.thread = threading.Thread(target=manager_suplies, args=(self.stop_event, self.pause_event, life_threshold, life_button, exura_threshold, exura_button, mana_threshold, mana_button))
+            self.thread = threading.Thread(target=manager_supplies, args=(self.stop_event, self.pause_event, life_threshold, life_button, exura_threshold, exura_button, mana_threshold, mana_button))
             self.thread.start()
+            self.start_button.config(state='disabled')
             self.root.iconify()  # Minimize the window
 
     def pause(self):
